9359340afc3601f4a822dcddaa08961ee0b5fa9f
Chunhawit <pupz28@gmail.com>
2024-12-15 20:24:11 +0100

    computed total balance

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Header from '@/components/Header.vue'
 import Balance from '@/components/Balance.vue'
 import IncomeExpenses from '@/components/IncomeExpenses.vue'
@@ -12,12 +12,18 @@ const transactions = ref([
   { id: 3, text: 'Book', amount: -10 },
   { id: 4, text: 'Camara', amount: -150 },
 ])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+})
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
