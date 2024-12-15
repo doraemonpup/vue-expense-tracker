1e1169b7a73f48918d989b4ed6e02601ee41c65d
Chunhawit <pupz28@gmail.com>
2024-12-15 12:47:57 +0100

    added IncomExpenses component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from '@/components/Header.vue'
 import Balance from '@/components/Balance.vue'
+import IncomeExpenses from '@/components/IncomeExpenses.vue'
 </script>
 
 <template>
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
   </div>
 </template>
