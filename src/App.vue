20f387c82ac3aae03598c6f54a6cafd0b091bf90
Chunhawit <pupz28@gmail.com>
2024-12-15 12:44:30 +0100

    added Header and Balance components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-<template>My App</template>
+<script setup>
+import Header from '@/components/Header.vue'
+import Balance from '@/components/Balance.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+  </div>
+</template>
